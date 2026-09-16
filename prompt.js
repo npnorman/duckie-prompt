5d6a38370d38f1535be69dd052f9511c5b49933f
npnorman <npnorman@iu.edu>
2026-09-15 20:20:55 -0400

    added new responses

--- prompt.js
+++ prompt.js
@@ -2,24 +2,31 @@
 var output = document.getElementById("output");
 var input = document.getElementById("input");
 
-var lines = [];
-
 input.addEventListener('keydown', function(e) {
     
+    lines = [
+        "This is interesting... Have you thought about that before like this?",
+        "What makes this problem so difficult?",
+        "This seems simply difficult, maybe a whiteboard would help?",
+        "*squeak* *squeak*",
+        "AMAZING IDEA (or maybe not), but at least you have it formally written down now."
+    ]
+
     if (e.key === "Enter") {
-        console.log("hi");
 
         let new_text = input.value;
 
-        input.textContent = "";
-
-        lines.push(new_text);
-
-        output.innerHTML = "Think about this as a phrasing for your questions...<br>";
-        for (let i = 0; i < lines.length; i++) {
-            output.innerHTML += lines[i];
-            output.innerHTML += "<br>";
+        if (new_text == "") {
+            return;
         }
+
+        input.value = "";
+
+        let randomElement = lines[Math.floor(Math.random() * lines.length)];
+        output.innerHTML += "<p class='human-output'>" + new_text + "<p>";
+        output.innerHTML += "<p class='yellow'><b>(Duckie)<br>" + randomElement + "</b></p>";
     }
 
+    let outputWrapper = document.getElementById("output-box");
+    outputWrapper.scrollTop = outputWrapper.scrollHeight;
 });
